b1081097474dfc47b230977386ae78062df3bf35
alekseykrazhev <alekseykrazhev@yandex.by>
2022-07-07 23:36:46 +0300

    correct project uploaded

--- Web Calendar/Topics/Error handlers/A JSON error handler/main.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, jsonify
-
-app = Flask(__name__)
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return jsonify({"msg": "Something was not found", "code": 404})
--- Web Calendar/Topics/Intro to templates and rendering/Templating/main.py
+++
@@ -1,9 +0,0 @@
-template = '''
-<!doctype html>
-<html>
-    <body>
-        <title>The Day of the Test</title>
-        <h1>Hold on, Holy Python, It's a day of the Flask</h1>
-    </body>
-</html>
-'''
